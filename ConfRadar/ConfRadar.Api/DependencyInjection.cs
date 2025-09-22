eda3421b8740758413c4c99a79f71fced1a12396
dai di <truongvi1511@gmail.com>
2025-09-22 13:44:26 +0700

    set up base project

---
+++ ConfRadar/ConfRadar.Api/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿namespace ConfRadar.Api
+{
+    public static class DependencyInjection
+    {
+        public static void  AddApiConfig(this IServiceCollection services,IConfiguration configs)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+        }
+    }
+}
---
+++ ConfRadar/ConfRadar.Api/Program.cs
@@ -0,0 +1,28 @@
+using ConfRadar.Api;
+using ConfRadar.Repositories;
+using ConfRadar.Services;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApiConfig(builder.Configuration);
+builder.Services.AddServices();
+builder.Services.AddRepositories();
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
