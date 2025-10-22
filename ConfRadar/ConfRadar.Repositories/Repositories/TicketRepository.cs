50b467275dfc34195fe2d7064bb1d2d5d2488cc1
anhdivippro <truongvi1511@gmail.com>
2025-10-22 13:06:19 +0700

    api view user registration&conference pagination

--- ConfRadar/ConfRadar.Repositories/Repositories/TicketRepository.cs
+++ ConfRadar/ConfRadar.Repositories/Repositories/TicketRepository.cs
@@ -14,6 +14,7 @@ namespace ConfRadar.Repositories.Repositories
     {
         Task<List<Ticket>> GetTicketsByUserId(string userId);
         Task<Ticket?> GetTicketByUserIdAndConferencePriceId(string userId, string conferencePriceId);
+        Task<List<Ticket>> GetTicketListByConferenceId(string conferenceId);
     }
     public class TicketRepository :GenericRepository<Ticket>,ITicketRepository
     {
@@ -28,6 +29,41 @@ namespace ConfRadar.Repositories.Repositories
         public async Task<Ticket?> GetTicketByUserIdAndConferencePriceId(string userId,string conferencePriceId)
         {
             return await _context.Tickets.FirstOrDefaultAsync(x => x.UserId == userId&& x.ConferencePriceId == conferencePriceId);
+        }
+        public async Task<List<Ticket>> GetTicketListByConferenceId(string conferenceId)
+        {
+            var query = from t in _context.Tickets
+                        join cp in _context.ConferencePrices on t.ConferencePriceId equals cp.ConferencePriceId
+                        join c in _context.Conferences on cp.ConferenceId equals c.ConferenceId
+                        join u in _context.Users on t.UserId equals u.UserId
+                        where t.IsRefunded == false && c.ConferenceId == conferenceId
+                        select new Ticket()
+                        {
+                            TicketId = t.TicketId,
+                            UserId = u.UserId,
+                            User = new User()
+                            {
+                                UserId = u.UserId,
+                                FullName = u.FullName,
+                                Email = u.Email,
+                                AvatarUrl = u.AvatarUrl,
+                            },
+                            ConferencePrice = new ConferencePrice
+                            {
+                                ConferencePriceId = cp.ConferencePriceId,
+                                ConferenceId = cp.ConferenceId,
+                                Conference = new Conference
+                                {
+                                    ConferenceId = c.ConferenceId,
+                                    ConferenceName = c.ConferenceName
+                                }
+                            },
+                            RegisteredDate = t.RegisteredDate,
+                            
+                        };
+            return await query.ToListAsync();
+
+
         }
     }
 }
--- ConfRadar/ConfRadar.Services/Services/TicketService.cs
+++ ConfRadar/ConfRadar.Services/Services/TicketService.cs
@@ -1,5 +1,6 @@
 ﻿using ConfRadar.Repositories;
 using ConfRadar.Repositories.Models;
+using ConfRadar.Services.DTOs.Ticket;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace ConfRadar.Services.Services
     public interface ITicketService
     {
         Task<List<Ticket>> GetTicketsByUserId(string userId);
+        Task<List<PaidTicketResponse>> GetTicketListByConferenceId(string conferenceId);
     }
     public class TicketService : ITicketService
     {
@@ -19,6 +21,27 @@ namespace ConfRadar.Services.Services
         {
             _unitOfWork = unitOfWork;
         }
+
+        public async Task<List<PaidTicketResponse>> GetTicketListByConferenceId(string conferenceId)
+        {
+            var tickets = await _unitOfWork.TicketRepository.GetTicketListByConferenceId(conferenceId);
+            if (tickets==null || tickets.Count <= 0)
+            {
+                return new List<PaidTicketResponse>();
+            }
+           return tickets.Select(x=> new PaidTicketResponse()
+           {
+               TicketId = x.TicketId,
+               UserId = x.UserId!,
+               UserName = x.User?.FullName?? "",
+               Email = x.User?.Email ?? "",
+               AvatarUrl = x.User?.AvatarUrl ?? "",
+               RegisteredDate = x.RegisteredDate ?? DateTime.Now,
+               ConferenceId = x.ConferencePrice?.ConferencePriceId ?? "",
+               ConferenceName = x.ConferencePrice.Conference.ConferenceName,
+           }).ToList();
+        }
+
         public Task<List<Ticket>> GetTicketsByUserId(string userId)
         {
             return _unitOfWork.TicketRepository.GetTicketsByUserId(userId);
