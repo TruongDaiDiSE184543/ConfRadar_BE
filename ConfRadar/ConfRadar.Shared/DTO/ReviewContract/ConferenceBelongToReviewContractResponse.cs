0d51e877c2b723ac47a9ce0276de471dd91c85af
ThienNHSE184989 <thiennhse184989@fpt.edu.vn>
2025-11-12 22:19:38 +0700

    change date to timestamp

--- ConfRadar/ConfRadar.Shared/DTO/ReviewContract/ConferenceBelongToReviewContractResponse.cs
+++ ConfRadar/ConfRadar.Shared/DTO/ReviewContract/ConferenceBelongToReviewContractResponse.cs
@@ -11,7 +11,7 @@
         public int? AvailableSlot { get; set; }
         public string? Address { get; set; }
         public string? BannerImageUrl { get; set; }
-        public DateOnly? CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
         public DateOnly? TicketSaleStart { get; set; }
         public DateOnly? TicketSaleEnd { get; set; }
         public bool? IsInternalHosted { get; set; }
