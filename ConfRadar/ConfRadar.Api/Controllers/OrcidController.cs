c3a3076863caeb263883d28fe1e4721dcf3efccd
ThienNHSE184989 <thiennhse184989@fpt.edu.vn>
2025-11-18 00:07:43 +0700

    orcid v1

---
+++ ConfRadar/ConfRadar.Api/Controllers/OrcidController.cs
@@ -0,0 +1,37 @@
+﻿using ConfRadar.Api.Responses;
+using ConfRadar.Services.DTOs.Orcid;
+using ConfRadar.Services.Exceptions;
+using ConfRadar.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfRadar.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrcidController : ControllerBase
+    {
+        private readonly IOrcidService _orcidService;
+
+        public OrcidController(IOrcidService orcidService)
+        {
+            _orcidService = orcidService;
+        }
+
+        [HttpGet("authorize-orcid")]
+        public async Task<IActionResult> AuthorizeOrcid()
+        {
+            string orcidOauth = _orcidService.GenerateAuthorizationLink();
+            return Ok(ApiResponse<string>.SuccessResponse(orcidOauth,"Lấy link oauth thành công"));
+        }
+
+        [HttpGet("callback")]
+        public async Task<IActionResult> ExchangeForAccessToken([FromQuery]string authorizationCode)
+        {
+            if (authorizationCode == null)
+                throw new BadRequestException($"Không tìm thấy code");
+            var tokenResponse = await _orcidService.ExchangeCodeForTokenAsync(authorizationCode);
+            return Ok(ApiResponse<OrcidAuthorizationResponse>.SuccessResponse(tokenResponse,""));
+        }
+
+    }
+}
