fc17f834b14ade275e20381bd46e735557339003
anhdivippro <truongvi1511@gmail.com>
2025-12-19 20:28:42 +0700

    remove bg jobs

--- ConfRadar/ConfRadar.Services/Services/PaperService.cs
+++ ConfRadar/ConfRadar.Services/Services/PaperService.cs
@@ -3219,9 +3219,9 @@ namespace ConfRadar.Services.Services
                 throw new NotFoundException($"Không tìm thấy giai đoạn nào diễn ra cho hội nghị {paper.Conference.ConferenceName}");
             }
             var dateNow = await _timeProviderService.GetVietnamDate();
-            if (dateNow < activeCurrentPhase.ReviseStartDate || dateNow > activeCurrentPhase.ReviseEndDate)
+            if (dateNow > activeCurrentPhase.ReviseEndDate)
             {
-                throw new BadRequestException($"Giai đoạn đánh dấu revise diễn ra từ {activeCurrentPhase.ReviseStartDate} đến {activeCurrentPhase.ReviseEndDate}");
+                throw new BadRequestException($"Giai đoạn đánh dấu revise hạn chót đến  {activeCurrentPhase.ReviseEndDate}");
             }
             if (paper.PaperPhaseId != currentRevisePhase.PaperPhaseId)
             {
