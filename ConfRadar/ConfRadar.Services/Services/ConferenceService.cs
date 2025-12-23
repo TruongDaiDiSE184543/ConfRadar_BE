879cba0e854882d52def6e55922afd0b1e7dcc92
ThienNHSE184989 <thiennhse184989@fpt.edu.vn>
2025-12-23 10:16:32 +0700

    fix change update exception error

--- ConfRadar/ConfRadar.Services/Services/ConferenceService.cs
+++ ConfRadar/ConfRadar.Services/Services/ConferenceService.cs
@@ -404,7 +404,7 @@ namespace ConfRadar.Services.Services
 
             if (invalidMessages.Any())
             {
-                string errorMessage = "Không thể chuyển về trạng thái 'Ready'. Các mốc thời gian sau đã bị lỗi thời và cần được cập nhật:\n- "
+                string errorMessage = "Không thể chuyển về trạng thái 'Ready'. VUi lòng sửa sau những vấn để sau:\n- "
                                     + string.Join("|", invalidMessages.Distinct());
                 throw new BadRequestException(errorMessage);
             }
@@ -553,7 +553,7 @@ namespace ConfRadar.Services.Services
 
             if (invalidMessages.Any())
             {
-                string errorMessage = "Không thể chuyển sang trạng thái 'Ready'. Vui lòng sửa những nơi có thời gian không hợp lệ:\n- "
+                string errorMessage = "Không thể chuyển sang trạng thái 'Ready'. Vui lòng sửa những vấn đề sau:\n- "
                                     + string.Join("|", invalidMessages.Distinct());
                 throw new BadRequestException(errorMessage);
             }
@@ -1373,7 +1373,7 @@ namespace ConfRadar.Services.Services
             if (conference.IsInternalHosted != true && conference.ConferenceStatusId == deleteStatus.ConferenceStatusId)
                 throw new Exception("Hội nghị được liên kết không thể chuyển sang trạng thái bị xoá, chỉ có thể tự động chuyển sang trạng thái này khi hợp đồng bị huỷ");
 
-            return UpdateConferenceStatusAsync(conferenceId, newStatusEntity.ConferenceStatusName!, reason).Result;
+            return await UpdateConferenceStatusAsync(conferenceId, newStatusEntity.ConferenceStatusName!, reason);
         }
 
 
